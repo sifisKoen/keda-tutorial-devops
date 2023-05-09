5a5cba133e40bca47fc01a56b0d9b26d92e99d7c
sifisKoen <sifis.k.i@gmail.com>
2023-05-09 14:22:40 +0200

    Add new files

---
+++ tutorial-keda/assets/server/app.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask, jsonify, render_template
+from flask_cors import CORS
+from prometheus_flask_exporter import PrometheusMetrics, Counter
+
+template_directory = os.path.abspath('src/templates')
+static_directory = os.path.abspath('src/static')
+
+app = Flask(__name__, template_folder=template_directory,
+            static_folder=static_directory)
+CORS(app)
+
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0')
+
+
+# counter = Counter('requests_total', 'Total number of requests')
+
+
+# @app.route('/metrics')
+# def prometheus_metrics():
+#     counter.inc()
+#     return metrics.export_metrics_as_latest(), 200
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/get_url', methods=['GET'])
+def get_url():
+    hardcoded_url = "https://www.google.com"
+    return jsonify({"url": hardcoded_url})
+
+
+HOST = '0.0.0.0'
+# 192.168.0.106
+# 130.229.178.24
+if __name__ == '__main__':
+    app.run(host=HOST, port=8080)
