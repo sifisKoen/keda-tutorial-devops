dacc095202d6b7ee4d7c2eee924f62a68c3c069a
sifisKoen <sifis.k.i@gmail.com>
2023-05-21 16:27:32 +0200

    Update K6 script with correctness

--- tutorial-keda/assets/k6-testing-users.js
+++ tutorial-keda/assets/k6-testing-users.js
@@ -1,14 +1,12 @@
 import http from 'k6/http';
 import { sleep } from 'k6';
 
-// 16 users
-// 300 seconds
 export let options = {
-    vus: 16, 
-    duration: '300s', 
+  vus: 16,
+  duration: '300s',
 };
 
 export default function () {
-    http.get('http://<your-service-url>'); // replace with your service URL. We can use the LOOPBACK address:<service-port>
-    sleep(1);
+  http.get('http://172.30.1.2:31386/get_url'); // replace with your service URL. We can use the LOOPBACK address:<service-port>
+  sleep(1);
 }
